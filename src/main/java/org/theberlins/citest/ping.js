da713a419b50a2979e30d7766795768b88f7bce5
Jonas Berlin <jonas@theberlins.org>
2018-09-18 15:45:59 -0400

    Changed message.

--- src/main/java/org/theberlins/citest/ping.js
+++ src/main/java/org/theberlins/citest/ping.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello There!');
+    res.end('Hello All of Yous!');
 }).listen(8080);
