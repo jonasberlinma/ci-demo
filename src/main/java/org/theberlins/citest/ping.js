c3b192d08534947758a463ab3c0e285ae76f92f5
Jonas Berlin <jonas@theberlins.org>
2018-09-18 13:40:36 -0400

    Changed world to there!

--- src/main/java/org/theberlins/citest/ping.js
+++ src/main/java/org/theberlins/citest/ping.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
+    res.end('Hello There!');
 }).listen(8080);
