59a713b9fcf242b335ef909b71a697d2878ec739
jonas <jonas@theberlins.org>
2018-11-21 21:31:10 -0500

    Refactor, break out ImageRecognitionRenderer from ImageRecognitionCaller

--- src/main/java/org/theberlins/citest/ImageRecognitionCaller.java
+++ src/main/java/org/theberlins/citest/ImageRecognitionCaller.java
@@ -1,18 +1,9 @@
 package org.theberlins.citest;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-
-import javax.swing.BorderFactory;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
-import javax.swing.border.BevelBorder;
 
 import org.json.simple.JSONObject;
 import org.slf4j.Logger;
@@ -25,11 +16,7 @@ public class ImageRecognitionCaller extends Thread {
 	private String URL;
 	private Logger log;
 	private boolean showGraphics;
-	private JFrame frame;
-	private JTextArea jta;
-	private String[] errorImage;
-	private String actual;
-	private String recognized;
+	private ImageRecognitionRenderer renderer;
 
 	public ImageRecognitionCaller(String URL, String dataFile, Logger log, boolean showGraphics) {
 		this.URL = URL;
@@ -37,41 +24,7 @@ public class ImageRecognitionCaller extends Thread {
 		this.log = log;
 		this.showGraphics = showGraphics;
 		if (showGraphics) {
-			frame = new JFrame("Image " + ImageRecognitionCaller.currentThread().getName());
-			jta = new JTextArea("<no data>");
-			jta.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
-			jta.setBounds(10, 10, 100, 40);
-			frame.add(jta);
-			frame.setSize(300, 300);
-		}
-		class MyPanel extends JPanel {
-			@Override
-			public void paint(Graphics g) {
-				if (errorImage != null) {
-					jta.setText("Actual=" + actual + "\nRecognized=" + recognized);
-					for (int i = 0; i < 28; i++) {
-						for (int j = 0; j < 28; j++) {
-							int point = j * 28 + i;
-							if (new Integer(errorImage[point]).intValue() > 0) {
-								int color = new Integer(errorImage[point]).intValue();
-								g.setColor(new Color(color, color, color));
-								g.fillRect(i * 10 + 10, j * 10 + 10, 10, 10);
-							}
-						}
-					}
-				}
-			}
-
-			@Override
-			public Dimension getPreferredSize() {
-				return new Dimension(300, 300);
-			}
-		}
-		if (showGraphics) {
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.add(new MyPanel());
-			frame.pack();
-			frame.setVisible(true);
+			renderer = new ImageRecognitionRenderer();
 		}
 	}
 
@@ -102,12 +55,9 @@ public class ImageRecognitionCaller extends Thread {
 				Long endTime = System.currentTimeMillis();
 				String errorMarker = "";
 				if (actualLabel.compareTo(res.getRecognizedValue()) != 0) {
-					errorImage = entries;
-					actual = actualLabel;
-					recognized = res.getRecognizedValue();
 					errorMarker = "<<<<<<<<<<<<";
 					if (showGraphics) {
-						frame.update(frame.getGraphics());
+						renderer.update(actualLabel, res.getRecognizedValue(), entries);
 					}
 				}
 				log.info("Sample=" + sampleNo + " Actual=" + actualLabel + " Predicted=" + res.getRecognizedValue()
---
+++ src/main/java/org/theberlins/citest/ImageRecognitionRenderer.java
@@ -0,0 +1,64 @@
+package org.theberlins.citest;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.border.BevelBorder;
+
+public class ImageRecognitionRenderer {
+	private JFrame frame;
+	private JTextArea jta;
+	private String[] errorImage;
+	private String actual;
+	private String recognized;
+	
+	ImageRecognitionRenderer(){
+		frame = new JFrame("Image " + ImageRecognitionCaller.currentThread().getName());
+		jta = new JTextArea("<no data>");
+		jta.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
+		jta.setBounds(10, 10, 100, 40);
+		frame.add(jta);
+		frame.setSize(300, 300);
+		class MyPanel extends JPanel {
+			
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void paint(Graphics g) {
+				if (errorImage != null) {
+					jta.setText("Actual=" + actual + "\nRecognized=" + recognized);
+					for (int i = 0; i < 28; i++) {
+						for (int j = 0; j < 28; j++) {
+							int point = j * 28 + i;
+							if (new Integer(errorImage[point]).intValue() > 0) {
+								int color = new Integer(errorImage[point]).intValue();
+								g.setColor(new Color(color, color, color));
+								g.fillRect(i * 10 + 10, j * 10 + 10, 10, 10);
+							}
+						}
+					}
+				}
+			}
+
+			@Override
+			public Dimension getPreferredSize() {
+				return new Dimension(300, 300);
+			}
+		}
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(new MyPanel());
+		frame.pack();
+		frame.setVisible(true);
+	}
+	public void update(String actual, String recognized, String[] image){
+		this.actual = actual;
+		this.recognized = recognized;
+		this.errorImage = image;
+		frame.update(frame.getGraphics());
+	}
+}
