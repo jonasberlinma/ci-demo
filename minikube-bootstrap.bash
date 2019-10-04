dfbdcd9bbd6476cfb97baa002c31662de1c0f542
Jonas Berlin <jonas@theberlins.org>
2019-10-04 18:04:29 -0400

    Added copy of certs

--- minikube-bootstrap.bash
+++ minikube-bootstrap.bash
@@ -8,3 +8,11 @@ kubectl create --save-config deployment image-recognizer-deployment --image=dock
 kubectl expose deployment image-recognizer-deployment --type=LoadBalancer --port=8080
 
 kubectl apply -f ingress.yaml
+
+docker cp ~/.minikube/ca.crt df9149f08bf8:/Users/jonas/.minikube/
+docker cp ~/.minikube/client.crt df9149f08bf8:/Users/jonas/.minikube/
+docker cp ~/.minikube/client.key df9149f08bf8:/Users/jonas/.minikube/
+
+docker exec -it -u 0 df9149f08bf8 chown -R 1000:1000 '/Users/jonas/.minikube/'
+
+
