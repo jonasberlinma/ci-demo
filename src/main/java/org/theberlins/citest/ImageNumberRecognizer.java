8639f2d50069eb549bd8396628031632d90dcc97
jonas <jonas@theberlins.org>
2019-10-04 12:28:03 -0400

    Triggering build

--- src/main/java/org/theberlins/citest/ImageNumberRecognizer.java
+++ src/main/java/org/theberlins/citest/ImageNumberRecognizer.java
@@ -6,7 +6,6 @@ import java.util.Set;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
