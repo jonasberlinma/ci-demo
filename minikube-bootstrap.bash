4533eab7b29244c8577ad91bcc31383c10bc051c
Jonas Berlin <jonas@theberlins.org>
2019-10-08 14:44:44 -0400

    Fixed

--- minikube-bootstrap.bash
+++ minikube-bootstrap.bash
@@ -1,18 +1,38 @@
 #!/bin/bash
 
-minikube delete
-minikube start -p minikube
+# Wipe the current cluster and create a new one
+minikube stop
 
+minikube delete
+minikube start -p minikube --vm-driver=virtualbox
+
+# Create the initial deployment
 kubectl create --save-config deployment image-recognizer-deployment --image=docker.io/jonasberlin/ci-demo:125
 
-kubectl expose deployment image-recognizer-deployment --type=LoadBalancer --port=8080
+# Expose the deployment in a new service
+kubectl expose deployment image-recognizer-deployment --type=NodePort --port=8080
 
-kubectl apply -f ingress.yaml
+# Enamble and configure ingress
+minikube addons enable ingress
 
-docker cp ~/.minikube/ca.crt df9149f08bf8:/Users/jonas/.minikube/
-docker cp ~/.minikube/client.crt df9149f08bf8:/Users/jonas/.minikube/
-docker cp ~/.minikube/client.key df9149f08bf8:/Users/jonas/.minikube/
+#kubectl apply -f ingress.yaml
 
-docker exec -it -u 0 df9149f08bf8 chown -R 1000:1000 '/Users/jonas/.minikube/'
+# Figure out the running docker-machine (not minikube's) which is running Jenkins
+eval $(docker-machine env dev)
 
+# Get the Jenkins container ID. This should be doable dynamically
+jenkins_container=df9149f08bf8
+
+# Copy the access keys to the docker
+docker exec -it -u 0 $jenkins_container mkdir -p /Users/jonas/.minikube
+
+docker cp ~/.minikube/ca.crt ${jenkins_container}:/Users/jonas/.minikube/
+docker cp ~/.minikube/client.crt ${jenkins_container}:/Users/jonas/.minikube/
+docker cp ~/.minikube/client.key ${jenkins_container}:/Users/jonas/.minikube/
+
+docker exec -it -u 0 $jenkins_container chown -R 1000:1000 '/Users/jonas/.minikube/'
+
+# Get the cluster IP and set it in /etc/hosts to make sure clients can connect through a predicable URL
+
+minikube status
 
